3338eafde36ca5bb9c2fa436e90178e6d2b8ddef
Amadeus <a.turner3x@gmail.com>
2023-05-24 00:52:01 -0400

    completed server.js file

--- server.js
+++ server.js
@@ -12,3 +12,44 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+  });
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+  });
+
+  // API Routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/Develop/db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+  
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/Develop/db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = generateUniqueId(); // Generate a unique ID for the new note
+      notes.push(newNote);
+      fs.writeFile(
+        path.join(__dirname, '/Develop/db/db.json'),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) throw err;
+          res.json(newNote);
+        }
+      );
+    });
+  });
+
+  // Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
